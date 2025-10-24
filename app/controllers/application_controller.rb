4a529691443108e98a08dc8c7cab21da0a192ec8
ko-ta-jpg <fksw122633@gmail.com>
2025-10-25 05:25:25 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?, unless: :healthcheck?
+  # 本番のみ Basic 認証、有効性チェック用の /up は除外
+  before_action :basic_auth,
+                if:     -> { Rails.env.production? },
+                unless: -> { request.path == "/up" }
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  def production?
-    Rails.env.production?
-  end
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      ActiveSupport::SecurityUtils.secure_compare(username, ENV.fetch("BASIC_AUTH_USER", "")) && 
-        ActiveSupport::SecurityUtils.secure_compare(password, ENV.fetch("BASIC_AUTH_PASSWORD", ""))
+      ActiveSupport::SecurityUtils.secure_compare(username.to_s, ENV.fetch("BASIC_AUTH_USER", "").to_s) &&
+        ActiveSupport::SecurityUtils.secure_compare(password.to_s, ENV.fetch("BASIC_AUTH_PASSWORD", "").to_s)
     end
   end
 
   def configure_permitted_parameters
-    added = [
-      :nickname,
-      :last_name, :first_name,
-      :last_name_kana, :first_name_kana,
-      # ▼ DBカラム名が :birthday なら :birthday にする（:birth_date ではなく）
-      :birthday
-    ]
+    added = [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday]
     devise_parameter_sanitizer.permit(:sign_up, keys: added)
     devise_parameter_sanitizer.permit(:account_update, keys: added)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+   get "/up", to: proc { [200, { "Content-Type" => "text/plain" }, ["OK"]] }
   root "items#index"
   resources :items
 end
