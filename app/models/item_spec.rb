6a569a7d52f2e262544cd38559ea715f1a63ed4a
ko-ta-jpg <fksw122633@gmail.com>
2025-10-29 22:47:20 +0900

    commit

--- app/models/item_spec.rb
+++ app/models/item_spec.rb
@@ -1,37 +1,37 @@
-require 'rails_helper'
+#require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  let(:user) { FactoryBot.create(:user) }
-  let(:item) { FactoryBot.build(:item, user:) }
+#RSpec.describe Item, type: :model do
+  #let(:user) { FactoryBot.create(:user) }
+  #let(:item) { FactoryBot.build(:item, user:) }
 
-  before { item.image.attach(io: File.open('spec/fixtures/files/sample.png'), filename: 'sample.png') }
+  #before { item.image.attach(io: File.open('spec/fixtures/files/sample.png'), filename: 'sample.png') }
 
-  context '出品できる' do
-    it '必須項目が揃えば有効' do
-      expect(item).to be_valid
-    end
+  #context '出品できる' do
+    #it '必須項目が揃えば有効' do
+     # expect(item).to be_valid
+    #end
   end
 
-  context '出品できない' do
-    it '画像必須' do
-      item.image = nil
-      item.valid?
-      expect(item.errors.full_messages).to include("Image can't be blank")
-    end
+ # context '出品できない' do
+   # it '画像必須' do
+    #  item.image = nil
+    #  item.valid?
+      #expect(item.errors.full_messages).to include("Image can't be blank")
+   # end
+#
+    #it '価格が空' do
+     # item.price = nil
+      #item.valid?
+    #  expect(item.errors.full_messages).to include("Price can't be blank")
+    #end
 
-    it '価格が空' do
-      item.price = nil
-      item.valid?
-      expect(item.errors.full_messages).to include("Price can't be blank")
-    end
+    #it '価格が範囲外（299）' do
+      #item.price = 299
+     # item.valid?
+      #expect(item.errors.full_messages).to include('Price must be greater than or equal to 300')
+    #end
 
-    it '価格が範囲外（299）' do
-      item.price = 299
-      item.valid?
-      expect(item.errors.full_messages).to include('Price must be greater than or equal to 300')
-    end
-
-    it '価格が範囲外（10000000）' do
+    #it '価格が範囲外（10000000）' do
       item.price = 10_000_000
       item.valid?
       expect(item.errors.full_messages).to include('Price must be less than or equal to 9999999')
