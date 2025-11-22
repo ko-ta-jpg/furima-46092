eacd88232088809a39e2dc3e0b9640d619993e4f
ko-ta-jpg <fksw122633@gmail.com>
2025-11-23 04:57:33 +0900

    commit

--- db/migrate/20251116123157_create_orders.rb
+++ db/migrate/20251116123157_create_orders.rb
@@ -1,14 +1,12 @@
-# frozen_string_literal: true
-
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
       t.references :user, null: false, foreign_key: true
-      # 1商品は1回しか購入できない前提ならユニーク制約を付ける
-      t.references :item, null: false, foreign_key: true, index: { unique: true }
-
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
+
+    # item_id にユニーク制約を付けたいならこれだけでOK
     add_index :orders, :item_id, unique: true
   end
 end
