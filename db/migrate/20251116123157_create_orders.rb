f571cfa1e89ba057a4c9c5ffbe519e4ee8e4e765
ko-ta-jpg <fksw122633@gmail.com>
2025-11-23 05:16:26 +0900

    commit

--- db/migrate/20251116123157_create_orders.rb
+++ db/migrate/20251116123157_create_orders.rb
@@ -5,8 +5,5 @@ class CreateOrders < ActiveRecord::Migration[7.1]
       t.references :item, null: false, foreign_key: true
       t.timestamps
     end
-
-    # item_id にユニーク制約を付けたいならこれだけでOK
-    add_index :orders, :item_id, unique: true
   end
 end
